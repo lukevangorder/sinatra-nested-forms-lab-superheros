c4f909df3e7c03227cc1ef3dba84c28f86c34364
lukevangorder <lukevangorder@gmail.com>
2021-02-24 18:34:04 -0500

    commit tree 1775026f37a4716aacd9b8864b51f596d0f41d5b

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,23 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :super_hero
+    end
+
+    post '/team' do
+        @team_name = params[:team][:name]
+        @team_motto = params[:team][:motto]
+        @member1_name = params[:member1_name]
+        @member1_power = params[:member1_power]
+        @member1_bio = params[:member1_bio]
+        @member2_name = params[:member2_name]
+        @member2_power = params[:member2_power]
+        @member2_bio = params[:member2_bio]
+        @member3_name = params[:member3_name]
+        @member3_power = params[:member3_power]
+        @member3_bio = params[:member3_bio]
+        erb :team
+    end
 
 end
